717191402f77016d7a5557dfde69894cd76122e1
Till Heinzel <tillheinzel@hotmail.com>
2019-09-07 07:19:33 +0200

    cleanup namespace

--- test/src/detail/is_node.hpp
+++ test/src/detail/is_node.hpp
@@ -1,16 +1,13 @@
 #pragma once
 
-using namespace tillh::pipes2;
-
-
 template<class T>
 struct is_node : std::false_type {};
 
 template<class Op, class Connections, class PrimaryConnection>
-struct is_node<Node<Op, Connections, PrimaryConnection>> : std::true_type {};
+struct is_node<tillh::pipes2::Node<Op, Connections, PrimaryConnection>> : std::true_type {};
 
 template<class T>
 constexpr auto is_node_v = is_node<T>::value;
 
 template<class Op, class Outputs>
-struct is_node<Output<Op, Outputs>> : std::true_type {};
+struct is_node<tillh::pipes2::Output<Op, Outputs>> : std::true_type {};
